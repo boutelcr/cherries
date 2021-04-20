a2c972bbc95f1076c06428ad5d436c9dea971ebe
webstede <webstede@rose-hulman.edu>
2021-04-20 11:56:40 -0400

    Add Image

--- ENGD250 Ethiopian App Project/src/P3Component.java
+++ ENGD250 Ethiopian App Project/src/P3Component.java
@@ -1,8 +1,33 @@
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.image.*;
+import java.io.File;
+import java.io.IOException;
+import java.awt.*;
 
-public class P3Component {
+public class P3Component extends JComponent {
+	BufferedImage img;
 
 	public P3Component() {
+		super();
 		// TODO Auto-generated constructor stub
 	}
 
+	public void importImage() {
+		BufferedImage img = null;
+		try {
+			img = ImageIO.read(new File("strawberry.jpg"));
+		} catch (IOException e) {
+		}
+		this.img = img; 
+	}
+
+	protected void paintComponent(Graphics g) {
+		// Asks the superclass to do its work
+		super.paintComponent(g);
+
+		Graphics2D graphics2 = (Graphics2D) g;
+		
+	}
 }
+
